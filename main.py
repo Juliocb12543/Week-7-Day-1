a4f283dec01e914689ae16aada36e256be15c26c
Julio Barrientos <113930475+Juliocb12543@users.noreply.github.com>
2022-10-04 17:22:35 +0000

    made loops

--- main.py
+++ main.py
@@ -7,11 +7,29 @@
 # Random in python
 # List comprehension
 #importing modules
+#moduralized your code
 #work on creating and understanding functions
 
-from decision_making import decisions
-from comparison_operators import comparison
-# from dictionaries_loops import dictionaries_loops
-
-decisions()
-comparison()
+# from decision_making import decisions
+# from comparison_operators import comparison
+# # from dictionaries_loops import dictionaries_loops
+from loops import loop
+# from enumerators import enumerator
+# from list_comprehension import list_comprehension
+# from logical_operators import logical_operators
+# from min_max import min_max
+# from random import random
+# from ranges import range
+# from while_loops import while_loop
+# from zip import zip
+# decisions()
+# comparison()
+loop()
+# enumerator()
+# list_comprehension()
+# logical_operators()
+# min_max()
+# random()
+# range()
+# while_loop()
+# zip()
